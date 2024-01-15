6f2396d8a557561048235d426ebfd8f0d018160e
Nandhukrishna m a <98420759+NANDHUKRISH@users.noreply.github.com>
2024-01-16 01:23:45 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,32 @@
 import React from 'react'
 
-function Card(props) {
+function PriceCard({data}) {
   return (
-    <>
-
-     <div className="col-lg-4">
-        <div className="card mb-5 mb-lg-0">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center fs-4">{props.info.plan}</h5>
-            <h6 className="card-price text-center">{props.info.price}<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-            <li className={props.info.isuser?"":"text-muted"}><span className="fa-li">{props.info.isuser?"✔️":"❌"}</span>{props.info.user}</li>
-            <li className={props.info.isstorage?"":"text-muted"}><span className="fa-li">{props.info.isstorage?"✔️":"❌"}</span>{props.info.storage}</li>
-            <li className={props.info.iscommunityaccess?"":"text-muted"}><span className="fa-li">{props.info.iscommunityaccess?"✔️":"❌"}</span>{props.info.communityaccess}</li>
-            <li className={props.info.ispublicproject?"":"text-muted"}><span className="fa-li">{props.info.ispublicproject?"✔️":"❌"}</span>{props.info.publicproject}</li>
-            <li className={props.info.isprivateproject?"":"text-muted"}><span className="fa-li">{props.info.isprivateproject?"✔️":"❌"}</span>{props.info.privateproject}</li>
-            <li className={props.info.isphonesupport?"":"text-muted"}><span className="fa-li">{props.info.isphonesupport?"✔️":"❌"}</span>{props.info.phonesupport}</li>
-            <li className={props.info.issubdomain?"":"text-muted"}><span className="fa-li">{props.info.issubdomain?"✔️":"❌"}</span>{props.info.subdomain}</li>
-            <li className={props.info.isreports?"":"text-muted"}><span className="fa-li">{props.info.isreports?"✔️":"❌"}</span>{props.info.reports}</li>
-            </ul>
-            <div className="d-grid">
-              <a href="#!" className="btn btn-primary text-uppercase">Button</a>
+   <>
+   <div className="col-lg-4">
+            <div className="card mb-5 mb-lg-0">
+              <div className="card-body">
+                <h5 className="card-title text-muted text-uppercase text-center">{data.plan}</h5>
+                <h6 className="card-price text-center">${data.price}<span className="period">/month</span></h6>
+                <hr/>
+                <ul className="fa-ul">
+                  <li className={data.isuser?"":"text-muted"} style={data.isbold?{fontWeight:"bold"}:null}><span className="fa-li">{data.isuser?"✔️":"❌"}</span>{data.user}</li>
+                  <li className={data.isstorage?"":"text-muted"}><span className="fa-li">{data.isstorage?"✔️":"❌"}</span>{data.storage}</li>
+                  <li className={data.ispublicproject?"":"text-muted"}><span className="fa-li">{data.ispublicproject?"✔️":"❌"}</span>{data.publicproject}</li>
+                  <li className={data.iscommunityaccess?"":"text-muted"}><span className="fa-li">{data.iscommunityaccess?"✔️":"❌"}</span>{data.communityaccess}</li>
+                  <li className={data.isprivateproject?"":"text-muted"}><span className="fa-li">{data.isprivateproject?"✔️":"❌"}</span>{data.privateproject}</li>
+                  <li className={data.isphonesupport?"":"text-muted"}><span className="fa-li">{data.isphonesupport?"✔️":"❌"}</span>{data.phonesupport}</li>
+                  <li className={data.issubdomain?"":"text-muted"}><span className="fa-li">{data.issubdomain?"✔️":"❌"}</span><b>{data.subdomainbold}</b>{data.subdomain}</li>
+                  <li className={data.isreports?"":"text-muted"}><span className="fa-li">{data.isreports?"✔️":"❌"}</span>{data.reports}</li>
+                </ul>
+                <div className="d-grid">
+                  <a href="!#" className="btn btn-primary text-uppercase">Button</a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    
-    </>
+   </>
   )
 }
-export default Card
+
+export default PriceCard
